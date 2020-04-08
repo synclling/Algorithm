1326bad074d9756e3be51389cd48c758335f4108
zard <1156721870@qq.com>
2020-04-08 23:28:36 +0800

    commit BalancedBinaryTree

---
+++ BalancedBinaryTree/BalancedBinaryTree/BalancedBinaryTree.cpp
@@ -0,0 +1,105 @@
+﻿#include "pch.h"
+#include "BalancedBinaryTree.h"
+
+void L_Rotate(BSTNode* &root)
+{
+	BSTNode* p = root->rchild;
+	root->rchild = p->lchild;
+	p->lchild = root;
+
+	root = p;
+}
+
+void R_Rotate(BSTNode* &root)
+{
+	BSTNode* p = root->lchild;
+	root->lchild = p->rchild;
+	p->rchild = root;
+
+	root = p;
+}
+
+int InsertAVL(BSTNode* &T, ElemType e, bool &taller)
+{
+	if (T == nullptr)
+	{
+		// 插入新结点，树"长高"，置taller为true
+		T = new BSTNode();
+		T->bf = EH;
+		T->data = e;
+		T->lchild = T->rchild = nullptr;
+		taller = true;
+	}
+	else
+	{
+		if (T->data == e)	// 树中已存在和e相同的结点
+		{
+			taller = false;
+			return FALSE;
+		}
+		else if(T->data > e) // 在T的左子树中搜索
+		{
+			if (!InsertAVL(T->lchild, e, taller))
+			{
+				return FALSE;
+			}
+
+			if (taller)
+			{
+				switch (T->bf)
+				{
+				case LH:
+				{
+
+				}
+				break;
+				case EH:
+				{
+					T->bf = LH;
+					taller = true;
+				}
+				break;
+				case RH:
+				{
+					T->bf = EH;
+					taller = false;
+				}
+				break;
+				}
+			}
+		}
+		else
+		{
+			if (!InsertAVL(T->rchild, e, taller))
+			{
+				return FALSE;
+			}
+
+			if (taller)
+			{
+				switch (T->bf)
+				{
+				case LH:
+				{
+					T->bf = EH;
+					taller = false;
+				}
+				break;
+				case EH:
+				{
+					T->bf = RH;
+					taller = true;
+				}
+				break;
+				case RH:
+				{
+
+				}
+				break;
+				}
+			}
+		}
+	}
+
+	return true;
+}
---
+++ BalancedBinaryTree/BalancedBinaryTree/BalancedBinaryTree.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#define TRUE  1
+#define FALSE 0
+
+#define LH +1	// 左高
+#define EH  0	// 等高
+#define RH -1	// 右高
+
+typedef int ElemType;
+
+typedef struct BinarySortTreeNode
+{
+	int	bf; // 结点的平衡因子
+	ElemType data;
+	BinarySortTreeNode*	lchild;
+	BinarySortTreeNode* rchild;
+} BSTNode;
+
+/*
+ *	@brief	对以*root为根节点的二叉排序树作左旋转处理
+ *	@param	[in/out]root	旋转之后root指向新的根节点
+ */
+void L_Rotate(BSTNode* &root);
+
+/*
+ *	@brief	对以*root为根节点的二叉排序树作右旋转处理
+ *	@param	[in/out]root	旋转之后root指向新的根节点
+ */
+void R_Rotate(BSTNode* &root);
+
+/*
+ *	@brief	若平衡的二叉排序树T中不存在和e相同的结点，则插入一个数据元素为e的新结点。
+ *	@param	[in]T		二叉排序树
+ *	@param	[in]e		新的数据元素
+ *	@param	[in]taller	反映T长高与否
+ *	@return 插入成功返回TRUE，否则返回FALSE
+ */
+int InsertAVL(BSTNode* &T, ElemType e, bool &taller);
