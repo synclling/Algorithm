f30914aa14cf5ab964f74909f840b0d5f9af9bf7
zard <1156721870@qq.com>
2020-04-10 23:42:18 +0800

    B-Tree project

---
+++ B-Tree/B-Tree/B-Tree.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "B-Tree.h"
+
+int Search(BTNode* p, KeyType k)
+{
+	int i = 1;
+	for (; i <= p->keynum && k >= p->key[i]; ++i); // 使得i:p->key[i] <= k < p->key[i + 1]
+	return i - 1;
+}
+
+Result SearchBTree(BTNode* T, KeyType k)
+{
+	Result res;
+
+	BTNode* p = T;
+	BTNode* q = nullptr;
+
+	int i = 0;
+	bool found = false;
+
+	while (p != nullptr && !found)
+	{
+		i = Search(p, k);
+		if (i > 0 && k == p->key[i])
+		{
+			found = true;
+		}
+		else
+		{
+			q = p;
+			p = p->ptr[i];
+		}
+	}
+
+	if (found)
+	{
+		res.pt = p;
+		res.i = i;
+		res.tag = 1;
+	}
+	else
+	{
+		res.pt = q;
+		res.i = i;
+		res.tag = 0;
+	}
+
+	return res;
+}
---
+++ B-Tree/B-Tree/B-Tree.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#define m 3	// B-树的阶
+
+typedef int KeyType;
+
+typedef struct BTreeNode
+{
+	int keynum;				// 结点中关键字个数
+	BTreeNode* parent;		// 指向双亲结点
+	KeyType key[m + 1];		// 关键字向量，0号单元不使用
+	BTreeNode* ptr[m + 1];	// 子树指针向量
+} BTNode;
+
+typedef struct Result
+{
+	BTreeNode* pt;			// 指向找到的结点
+	int i;					// 关键字在结点中的位置
+	int tag;				// 1:查找成功，0:查找失败
+} Result;
+
+/*
+ *	@brief	在m阶的B-树上查找关键字k，返回结果(pt, i, tag)
+ *	@param	[in]T		B-树
+ *	@param	[in]k		关键字k
+ *	@return 查找成功tag = 1，指针pt所指结点中的第i个关键字等于k；否则tag = 0。
+ */
+Result SearchBTree(BTNode* T, KeyType k);
