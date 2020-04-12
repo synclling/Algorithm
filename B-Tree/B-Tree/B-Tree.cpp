917b2b09cfa28e7f160a4ac98c66a5c61a9a33a5
zard <1156721870@qq.com>
2020-04-12 23:48:59 +0800

    update B-Tree

--- B-Tree/B-Tree/B-Tree.cpp
+++ B-Tree/B-Tree/B-Tree.cpp
@@ -47,35 +47,3 @@ Result SearchBTree(BTNode* T, KeyType k)
 
 	return res;
 }
-
-//void InsertBTree(BTree &t, int i, KeyType k, BTNode *p) {
-//	BTNode *q;
-//	int finish_tag, newroot_tag, s;                   //设定需要新结点标志和插入完成标志 
-//	KeyType x;
-//	if (p == NULL)                                     //t是空树
-//		NewRoot(t, k, NULL, NULL);                     //生成仅含关键字k的根结点t
-//	else {
-//		x = k;
-//		q = NULL;
-//		finish_tag = 0;
-//		newroot_tag = 0;
-//		while (finish_tag == 0 && newroot_tag == 0) {
-//			InsertBTNode(p, i, x, q);                  //将关键字x和结点q分别插入到p->key[i+1]和p->ptr[i+1]
-//			if (p->keynum <= Max)
-//				finish_tag = 1;                       //插入完成
-//			else {
-//				s = (m + 1) / 2;
-//				SplitBTNode(p, q);                 //分裂结点 
-//				x = p->key[s];
-//				if (p->parent) {                      //查找x的插入位置
-//					p = p->parent;
-//					i = SearchBTNode(p, x);
-//				}
-//				else                            //没找到x，需要新结点 
-//					newroot_tag = 1;
-//			}
-//		}
-//		if (newroot_tag == 1)                      //根结点已分裂为结点p和q 
-//			NewRoot(t, x, p, q);                   //生成新根结点t,p和q为子树指针
-//	}
-//}
