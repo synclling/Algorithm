2fe8aab487f3a514b88656149f82428f4b3c8989
synclling <1156721870@qq.com>
2020-07-09 20:44:41 +0800

    red black tree

--- RedBlackTree/RedBlackTree/RedBlackTree.cpp
+++ RedBlackTree/RedBlackTree/RedBlackTree.cpp
@@ -15,7 +15,77 @@ void rbtree_init(rbtree *tree, rbtree_node *s, rbtree_insert_pt i)
 
 void rbtree_insert(rbtree *tree, rbtree_node *node)
 {
+	rbtree_node *temp = NULL;
 
+	rbtree_node **root = &(tree->root);
+	rbtree_node *sentinel = tree->sentinel;
+
+	if (*root == sentinel)	// 根结点为空
+	{
+		node->parent = NULL;
+		node->left = sentinel;
+		node->right = sentinel;
+		rbt_black(node);	// 根结点必须为黑色
+		*root = node;
+
+		return;
+	}
+
+	tree->insert(*root, node, sentinel);	// 插入结点
+
+	while (node != *root && rbt_is_red(node->parent))
+	{
+		if (node->parent == node->parent->parent->left)
+		{
+			rbtree_node *temp = node->parent->parent->right;
+
+			if (rbt_is_red(temp)) // 叔叔结点为红色
+			{
+				rbt_black(node->parent);
+				rbt_black(temp);
+				rbt_red(node->parent->parent);
+				node = node->parent->parent;
+			}
+			else
+			{
+				if (node == node->parent->right)
+				{
+					node = node->parent;
+					rbtree_left_rotate(root, sentinel, node);
+				}
+
+				rbt_black(node->parent);
+				rbt_red(node->parent->parent);
+				rbtree_right_rotate(root, sentinel, node->parent->parent);
+			}
+		}
+		else
+		{
+			rbtree_node *temp = node->parent->parent->left;
+
+			if (rbt_is_red(temp))	// 叔叔结点为红色
+			{
+				rbt_black(node->parent);
+				rbt_black(temp);
+				rbt_red(node->parent->parent);
+				node = node->parent->parent;
+			}
+			else
+			{
+				if (node == node->parent->left)
+				{
+					node = node->parent;
+					rbtree_right_rotate(root, sentinel, node);
+				}
+
+				rbt_black(node->parent);
+				rbt_red(node->parent->parent);
+				rbtree_left_rotate(root, sentinel, node->parent->parent);
+			}
+		}
+	}
+
+	rbt_black(*root);
 }
 
 void rbtree_delete(rbtree *tree, rbtree_node *node)
@@ -37,6 +107,7 @@ static inline void rbtree_left_rotate(rbtree_node **root, rbtree_node *sentinel,
 	}
 
 	temp->parent = node->parent;
+
 	if (node == *root)
 	{
 		*root = temp;
@@ -66,6 +137,7 @@ static inline void rbtree_right_rotate(rbtree_node **root, rbtree_node *sentinel
 	}
 
 	temp->parent = node->parent;
+
 	if (node == *root)
 	{
 		*root = temp;
