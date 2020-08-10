2822fb73d54cf6ba5c0808f5d34da0cdfc0035a3
synclling <1156721870@qq.com>
2020-08-10 23:38:51 +0800

    collecting apples

--- DynamicProgramming/CollectingApples/CollectingApples.cpp
+++ DynamicProgramming/CollectingApples/CollectingApples.cpp
@@ -9,11 +9,21 @@ int apples_count(int **a, int **dp, int n, int m)
 
 	for (i = 1; i < n; ++i)
 	{
-		dp[0][i] = a[0][i - 1] + a[0][i];
+		dp[0][i] = dp[0][i - 1] + a[0][i];
 	}
 
 	for (j = 1; j < m; ++j)
 	{
-		
+		dp[j][0] = dp[j - 1][0] + a[j][0];
 	}
+
+	for (i = 1; i < n; ++i)
+	{
+		for (j = 1; j < m; ++j)
+		{
+			dp[i][j] = (dp[i - 1][j] > dp[i][j - 1]) ? (dp[i - 1][j] + a[i][j]) : (dp[i][j - 1] + a[i][j]);
+		}
+	}
+
+	return dp[m - 1][n - 1];
 }
