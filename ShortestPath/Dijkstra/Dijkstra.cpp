9e1646d6509189e1498c4bb50d8d1b8d987b6fa1
synclling <1156721870@qq.com>
2020-08-24 23:52:23 +0800

    Dijkstra

--- ShortestPath/Dijkstra/Dijkstra.cpp
+++ ShortestPath/Dijkstra/Dijkstra.cpp
@@ -1,1 +1,46 @@
-﻿
+﻿#include "Dijkstra.h"
+#include <iostream>
+
+int LocateVex(const MGraph &G, VertexType v)
+{
+	for (int i = 0; i < G.vexnum; ++i)
+	{
+		if (v == G.vexs[i])	return i;
+	}
+
+	return -1;
+}
+
+void CreateMGraph(MGraph &G)
+{
+	int i, j, k;
+	std::cout << "请输入图的顶点数和弧数: ";
+	std::cin >> G.vexnum >> G.arcnum;
+
+	std::cout << "请输入图的顶点: ";
+	for (i = 0; i < G.vexnum; ++i)
+	{
+		std::cin >> G.vexs[i];
+	}
+
+	// 初始化邻接矩阵 INT_MAX
+	for (i = 0; i < G.vexnum; ++i)
+	{
+		for (j = 0; j < G.vexnum; ++j)
+		{
+			G.arcs[i][j].adj = INTEGER_MAX;
+		}
+	}
+
+	VRType w;
+	VertexType vi, vj;
+	for (k = 0; k < G.arcnum; ++k)
+	{
+		std::cout << "请输入弧(vi, vj)的顶点vi, vj以及相应的权值: ";
+		std::cin >> vi >> vj >> w;
+		i = LocateVex(G, vi);
+		j = LocateVex(G, vj);
+
+		G.arcs[i][j].adj = G.arcs[j][i].adj = w;
+	}
+}
--- ShortestPath/Dijkstra/Dijkstra.h
+++ ShortestPath/Dijkstra/Dijkstra.h
@@ -1,1 +1,27 @@
 ﻿#pragma once
+
+#define MAX_VERTEX_NUM		20
+#define INTEGER_MAX			0xffff
+
+// -----图的邻接矩阵存储表示-----
+
+// VRType: 顶点关系类型。对于无权图，用1或0表示是否相邻；对于带权图，为权值类型。
+typedef int		VRType;
+// VertexType: 顶点类型
+typedef char	VertexType;
+
+typedef struct ArcCell
+{
+	VRType	adj;			// VRType顶点关系类型。对于带权图，为权值类型。
+	/*InfoType *info;		// 该弧相关信息的指针*/
+} ArcCell, AdjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+
+typedef struct MGraph
+{
+	/*GraphKind	kind;					// 图的类型: 有向图、无向图、有向网、无向网*/
+	int	vexnum, arcnum;					// 图的当前顶点数和弧数
+	VertexType	vexs[MAX_VERTEX_NUM];	// 顶点向量
+	AdjMatrix	arcs;					// 弧的邻接矩阵
+} MGraph;
+
+void CreateMGraph(MGraph &G);
