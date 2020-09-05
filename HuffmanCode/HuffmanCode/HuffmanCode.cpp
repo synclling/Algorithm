efa5b11fd44bbca6d6911585440795b9d8d4bdab
synclling <1156721870@qq.com>
2020-09-05 23:43:08 +0800

    Huffman code

--- HuffmanCode/HuffmanCode/HuffmanCode.cpp
+++ HuffmanCode/HuffmanCode/HuffmanCode.cpp
@@ -1,5 +1,5 @@
-#include "pch.h"
 #include "HuffmanCode.h"
+#include <iostream>
 
 int min_weight(HuffmanTree &HT, int n)
 {
@@ -22,72 +22,109 @@ void select(HuffmanTree &HT, int n, int &s1, int &s2)
 	s2 = min_weight(HT, n);
 }
 
-void HuffmanCoding(HuffmanTree &HT, HuffmanCode &HC, int* w, int n)
+void InitHuffmanTree(HuffmanTree &HT, int *w, int n)
 {
-	if (n <= 1)
-	{
-		return;
-	}
 
-	int i, m;
-	int s1, s2;
-	int start, c, f;
-	HuffmanTree p;
-	m = 2 * n - 1;
-	HT = (HuffmanTree)malloc((m + 1) * sizeof(HTNode));	//0号单元未使用 
-	if (!HT)
-	{
-		printf("HuffmanTree malloc faild!\n");
-		exit(-2);
-	}
+}
 
-	//初始化
-	for (p = HT + 1, i = 1; i <= n; ++i, ++p, ++w)
-	{	//1 ~ n单元  
+void HuffmanCoding(HuffmanTree &HT, HuffmanCode &HC, int *w, int n)
+{
+	if (n <= 1)	return;
+
+	int i;
+
+	// 分配内存
+	int m = 2 * n - 1;
+	HT = (HuffmanTree)malloc((m + 1) * sizeof(HTNode)); // 0号单元未用
+	
+	// 初始化1 ~ n
+	HuffmanTree p = &HT[1];
+	for (i = 1; i <= n; ++i, ++p, ++w)
+	{
 		p->weight = *w;
 		p->parent = 0;
 		p->lchild = 0;
 		p->rchild = 0;
 	}
-	for (; i <= m; ++i, ++p)
-	{	//n + 1 ~ m单元 
+
+	// 初始化n+1 ~ m
+	for (; i <= m; ++i)
+	{
 		p->weight = 0;
 		p->parent = 0;
 		p->lchild = 0;
 		p->rchild = 0;
 	}
 
-	//建赫夫曼树 
-	for (i = n + 1; i <= m; ++i)
-	{
-		select(HT, i - 1, s1, s2);
-		HT[s1].parent = i;	HT[s2].parent = i;
-		HT[i].lchild = s1;	HT[i].rchild = s2;
-		HT[i].weight = HT[s1].weight + HT[s2].weight;
-	}
 
-	//-----从叶子到根逆向求每个字符的赫夫曼编码-----
-	HC = (HuffmanCode)malloc((n + 1) * sizeof(char *));
-	if (!HC)
-	{
-		printf("HuffmanCode malloc faild!\n");
-		exit(-2);
-	}
-	char *code = (char *)malloc(n * sizeof(char));
-	if (!code)
-	{
-		printf("code malloc faild!\n");
-		exit(-2);
-	}
-	code[n - 1] = '\0';
-	for (i = 1; i <= n; ++i)
-	{
-		start = n - 1;
-		for (c = i, f = HT[i].parent; f != 0; c = f, f = HT[f].parent)
-			if (HT[f].lchild == c)	code[--start] = '0';
-			else	code[--start] = '1';
-		HC[i] = (char *)malloc((n - start) * sizeof(char));
-		strcpy(HC[i], &code[start]);
-	}
-	free(code);
+
 }
+//void HuffmanCoding(HuffmanTree &HT, HuffmanCode &HC, int *w, int n)
+//{
+//	if (n <= 1)
+//	{
+//		return;
+//	}
+//
+//	int i, m;
+//	int s1, s2;
+//	int start, c, f;
+//	HuffmanTree p;
+//	m = 2 * n - 1;
+//	HT = (HuffmanTree)malloc((m + 1) * sizeof(HTNode));	//0号单元未使用 
+//	if (!HT)
+//	{
+//		printf("HuffmanTree malloc faild!\n");
+//		exit(-2);
+//	}
+//
+//	//初始化
+//	for (p = HT + 1, i = 1; i <= n; ++i, ++p, ++w)
+//	{	//1 ~ n单元  
+//		p->weight = *w;
+//		p->parent = 0;
+//		p->lchild = 0;
+//		p->rchild = 0;
+//	}
+//	for (; i <= m; ++i, ++p)
+//	{	//n + 1 ~ m单元 
+//		p->weight = 0;
+//		p->parent = 0;
+//		p->lchild = 0;
+//		p->rchild = 0;
+//	}
+//
+//	//建赫夫曼树 
+//	for (i = n + 1; i <= m; ++i)
+//	{
+//		select(HT, i - 1, s1, s2);
+//		HT[s1].parent = i;	HT[s2].parent = i;
+//		HT[i].lchild = s1;	HT[i].rchild = s2;
+//		HT[i].weight = HT[s1].weight + HT[s2].weight;
+//	}
+//
+//	//-----从叶子到根逆向求每个字符的赫夫曼编码-----
+//	HC = (HuffmanCode)malloc((n + 1) * sizeof(char *));
+//	if (!HC)
+//	{
+//		printf("HuffmanCode malloc faild!\n");
+//		exit(-2);
+//	}
+//	char *code = (char *)malloc(n * sizeof(char));
+//	if (!code)
+//	{
+//		printf("code malloc faild!\n");
+//		exit(-2);
+//	}
+//	code[n - 1] = '\0';
+//	for (i = 1; i <= n; ++i)
+//	{
+//		start = n - 1;
+//		for (c = i, f = HT[i].parent; f != 0; c = f, f = HT[f].parent)
+//			if (HT[f].lchild == c)	code[--start] = '0';
+//			else	code[--start] = '1';
+//		HC[i] = (char *)malloc((n - start) * sizeof(char));
+//		strcpy(HC[i], &code[start]);
+//	}
+//	free(code);
+//}
--- HuffmanCode/HuffmanCode/HuffmanCode.h
+++ HuffmanCode/HuffmanCode/HuffmanCode.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define UINTMAX 65535
+//-----Huffman编码-----
+
+// 设计长短不等的编码，则必须是任意一个字符的编码都不是其他字符的编码的前缀，这种编码称为前缀编码
 
 typedef struct HuffmanTreeNode
 {
@@ -14,7 +16,7 @@ typedef char** HuffmanCode;
  *	@brief	构造Huffman树HT，并求出n个字符的Huffman编码
  *	@param	[out]HT		Huffman树
  *	@param	[out]HC		Huffman编码
- *	@param	[in]w		存放n个字符的权值数组
+ *	@param	[in]w		w存放n个字符的权值(均大于0)
  *	@param	[in]n		n个字符
  */
-void HuffmanCoding(HuffmanTree &HT, HuffmanCode &HC, int* w, int n);
+void HuffmanCoding(HuffmanTree &HT, HuffmanCode &HC, int *w, int n);
